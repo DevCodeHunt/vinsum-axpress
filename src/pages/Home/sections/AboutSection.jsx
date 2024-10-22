176da3378de1d5f8634f6ee85e07c35fbc22480b
PavitarSharma <pavitarsharma144@gmail.com>
2024-10-22 11:34:51 +0530

    Add navigation on about card

--- src/pages/Home/sections/AboutSection.jsx
+++ src/pages/Home/sections/AboutSection.jsx
@@ -3,8 +3,20 @@ import Card from "../../../components/Card";
 import { aboutCards } from "../../../constants";
 import { motion } from "framer-motion";
 import { fadeIn, fadeInOpacity } from "../../../utils/motion";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../utils/routes";
 
 const AboutSection = () => {
+  const navigate = useNavigate();
+  const handleClick = (index) => {
+    if (index === aboutCards.length - 1) {
+      navigate(ROUTES.CONTACT);
+    } else if (index === 0) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      navigate(ROUTES.SERVICE);
+    }
+  };
   return (
     <section className="wrapper my-10 mt-24">
       <Card hasBg>
@@ -26,7 +38,9 @@ const AboutSection = () => {
             variants={fadeIn("right", "tween", 0.2, 1)}
             className="card-description"
           >
-            Innovative Solutions for Every Journey: From real-time tracking to customized logistics plans, we adapt to your needs for maximum efficiency and transparency.
+            Innovative Solutions for Every Journey: From real-time tracking to
+            customized logistics plans, we adapt to your needs for maximum
+            efficiency and transparency.
           </motion.p>
         </div>
         <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4 mt-10">
@@ -35,6 +49,7 @@ const AboutSection = () => {
               <motion.div
                 variants={fadeInOpacity(index * 0.6)}
                 key={index}
+                onClick={() => handleClick(index)}
                 className={`bg-white  border drop-shadow w-full rounded-2xl p-6 space-y-4 hover:bg-primary hover:text-white transition duration-300 group cursor-pointer`}
               >
                 <h2 className="font-bold text-4xl">O{index + 1}</h2>
